f50c7081963163c3d9f6d1c39ee570f764262f9a
mscope <dev@mscope.app>
2026-07-06 13:37:14 +0800

    fix entrypoint: remove stale -data-listen flag

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,10 +1,7 @@
 #!/bin/sh
 set -e
 
-# Allow config via env vars
 CENTRAL_ADDR="${CENTRAL_ADDR:-central:38472}"
-DATA_LISTEN="${DATA_LISTEN:-0.0.0.0:443}"
 
 exec mscope-edge \
-  -central-addr "$CENTRAL_ADDR" \
-  -data-listen "$DATA_LISTEN"
+  -central-addr "$CENTRAL_ADDR"
